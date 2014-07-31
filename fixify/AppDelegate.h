e9ee4f3c52c9a07f6301f24efebb93779b2ea335
aliya <aliya@qburst.com>
2014-07-31 17:58:42 +0530

    Created Fixify

---
+++ fixify/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  fixify
+//
+//  Created by qbadmin on 31/07/14.
+//  Copyright (c) 2014 qburst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ fixifyTests/fixifyTests.m
@@ -0,0 +1,34 @@
+//
+//  fixifyTests.m
+//  fixifyTests
+//
+//  Created by qbadmin on 31/07/14.
+//  Copyright (c) 2014 qburst. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface fixifyTests : XCTestCase
+
+@end
+
+@implementation fixifyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
