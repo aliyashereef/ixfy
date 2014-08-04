be772101daa73b93fdf74f6200a1c7045e25be11
aliya <aliya@qburst.com>
2014-08-04 16:43:34 +0530

    Landing screen implemented

---
+++ fixify/AppTourCell.m
@@ -0,0 +1,31 @@
+//
+//  AppTourCell.m
+//  fixify
+//
+//  Created by qbadmin on 04/08/14.
+//  Copyright (c) 2014 qburst. All rights reserved.
+//
+
+#import "AppTourCell.h"
+
+@implementation AppTourCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
